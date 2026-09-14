6117490c774022e519093f6e24000f24adfa0f4b
peter351180828-design <peter351180828@gmail.com>
2026-09-14 11:01:33 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@
  */
 window.PHOTO_ARCHIVE_CONFIG = {
   supabaseUrl: 'https://kspatelbqtoyotxtadqn.supabase.co',
-  supabaseKey: 'sb_publishable_yrHi4_DDO10AE-sfp2Sd5Q_7voGT7PaE',
+  supabaseKey: 'sb_publishable_yrHi4_DDO10AE-sfp2Sd5Q_7voGT7Pa',
   siteName: '咸鱼桑',
   siteSubtitle: '摄影档案',
   englishName: 'Conzie',
